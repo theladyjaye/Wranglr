288eb70cdc02ad7e949ec4f3bb33b2fc3f3dacc4
Aubrey <aubby.net@gmail.com>
2011-12-11 17:33:42 -0800

    adding svg spike, added gitignore

---
+++ spikes/svg/resources/js/wranglr-global-application.js
@@ -0,0 +1,18 @@
+var Wranglr = Wranglr || {};
+Wranglr.Application = function(controller){
+
+	var self = {}
+
+	function __new__(){
+	
+	}
+
+	function __init__(){
+		self.controller  = controller();
+	}
+	
+	__new__();
+	__init__();
+
+	return self;
+}
